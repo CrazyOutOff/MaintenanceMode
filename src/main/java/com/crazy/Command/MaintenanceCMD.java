3f835588882cbba07c636be80fb41d7d675f780f
CrazyOutOff <dadouneprank@gmail.com>
2023-05-16 00:01:52 +0200

    Add config.yml file

---
+++ src/main/java/com/crazy/Command/MaintenanceCMD.java
@@ -0,0 +1,21 @@
+package com.crazy.Command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.io.File;
+
+public class MaintenanceCMD implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            player.sendMessage("Hello world");
+        }
+        return false;
+    }
+}
--- src/main/java/com/crazy/Main.java
+++ src/main/java/com/crazy/Main.java
@@ -1,7 +1,57 @@
 package com.crazy;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import com.crazy.Command.MaintenanceCMD;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+
+public class Main extends JavaPlugin {
+
+    public static Main instance;
+
+    public String prefix = "MaintenanceMode | ";
+
+    public Logger logger = this.getLogger();
+
+    private File customConfigFile;
+    private FileConfiguration customConfig;
+
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info( prefix + "Initializing plugin");
+
+        this.getCommand("maintenance").setExecutor(new MaintenanceCMD());
+
+        createCustomConfig();
+    }
+    public static Main getInstance() {
+        return instance;
+    }
+
+    public Logger logger() {return logger;}
+
+    private void createCustomConfig() {
+        customConfigFile = new File(getDataFolder(), "config.yml");
+        if (!customConfigFile.exists()) {
+            customConfigFile.getParentFile().mkdirs();
+            saveResource(customConfigFile.getName(), true);
+        }
+
+        customConfig = new YamlConfiguration();
+        try {
+            customConfig.load(customConfigFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+    public FileConfiguration getCustomConfig() {
+        return this.customConfig;
     }
 }
